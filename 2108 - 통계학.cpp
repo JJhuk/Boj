49b3a2ff6127e881bb88cb2323274566ecd25616
JJhuk <31194565+JJhuk@users.noreply.github.com>
2019-05-13 21:31:58 +0900

    Update 2108 - 통계학.cpp
    잘몰올린거 수정 + 주석 조금 달아놨어요

--- 2108 - 통계학.cpp
+++ 2108 - 통계학.cpp
@@ -20,39 +20,42 @@ int main(void) {
 	int count = 0;
 	vector<int> v;
 	vector<int> mode_save;
-	for (int i = 0; i < n; i++) {
+	
+	for (int i = 0; i < n; i++) {	//합과 vector안에 원소를 집어넣는과정
 		cin >> inPut;
 		v.push_back(inPut);
 		sum += inPut;
 	}
-	arithmetic_mean = (double)sum / (double)n;
-	if (arithmetic_mean > 0)
+	
+	arithmetic_mean = (double)sum / (double)n;	//산술평균 구하는 과정
+	if (arithmetic_mean > 0)	//반올림
 		arithmetic_mean += 0.5;
 	else
 		arithmetic_mean -= 0.5;
 
-	sort(v.begin(), v.end());
-	middle = v.at((n - (n / 2)) - 1);
+	sort(v.begin(), v.end());	//정렬
+	
+	middle = v.at((n - (n / 2)) - 1);	//정렬 후 중앙값
 
-	for (int i = 0; i < v.size(); i++)
+	for (int i = 0; i < v.size(); i++)	//각 인덱스가 숫자를 의미함
 		count_mode[v[i] + 4000]++;
 	for (int i = 0; i <= 8000; i++)
 		max = max > count_mode[i] ? max : count_mode[i];
 	for (int i = 0; i <= 8000; i++) {
 		if (count_mode[i] == max) {
-			mode_save.push_back(i - 4000);
+			mode_save.push_back(i - 4000);	//각 인덱스를 vector에 집어넣음
 		}
 	}
 
-	sort(mode_save.begin(), mode_save.end());
+	sort(mode_save.begin(), mode_save.end());	//혹시 최빈값이 중복일때를 위해!
 
-	if (mode_save.size() == 1)
+	if (mode_save.size() == 1)	//벡터의 원소가 1개면 front
 		mode = mode_save.front();
-	else
+	else	//벡터의 원소가 2개 이상일때 2번째로 작은 숫자
 		mode = mode_save.at(1);
 
 
-	range = *max_element(v.begin(), v.end()) - *min_element(v.begin(), v.end());
+	range = *max_element(v.begin(), v.end()) - *min_element(v.begin(), v.end());	//백터의 최대와 최소의 차
 
 	cout << (int)arithmetic_mean << "\n" << middle << "\n" << mode << "\n" << range;
 
